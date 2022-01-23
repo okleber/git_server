852948f3dbdacbc141465d61affa306cc1021e01
kleber <okleber@gmail.com>
2022-01-23 10:49:55 +0100

    update

--- app/main.py
+++ app/main.py
@@ -4,18 +4,24 @@ import glob
 
 app = FastAPI()
 
-@app.get("/repolist")
-async def root():
+@app.get("/listrepo")
+async def listrepo():
     dir_list = glob.glob("/*.git",)
     return {" ": dir_list}
 
 @app.post("/createrepo/{repo_name}")
 async def createrepo(repo_name: str):
-    subprocess.check_call(["mkdir", f"/{repo_name}.git"])
-    output=subprocess.check_output(["git","init","--bare",f"/{repo_name}.git"])
-    return {"output": output}
+    try:
+        subprocess.check_call(["mkdir", f"/{repo_name}.git"])
+        output=subprocess.check_output(["git","init","--bare",f"/{repo_name}.git"])
+        return {"output": output}
+    except:
+        pass
 
 @app.post("/deleterepo/{repo_name}")
 async def deleterepo(repo_name: str):
-    output=subprocess.check_output(["rm", "-rf", f"/{repo_name}.git"])
-    return {"output": output}
+    try:
+        subprocess.check_call(["rm", "-rf", f"/{repo_name}.git"])
+        return {"output": f"Deleted {repo_name}"}
+    except:
+        pass
